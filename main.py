5b550cae3bcd3dd5c5baa12c55513787c78d3977
Jason Luc <lucjason+github@icloud.com>
2025-10-04 13:27:10 +0200

    add POST endpoint to create items

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return item
